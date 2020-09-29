85365db18212fc349d487b5c00609a8e4d1023b2
Egor <lalalayes2@gmail.com>
2020-09-29 19:21:23 +0300

    feat: morse-decoder complete

--- src/index.js
+++ src/index.js
@@ -39,6 +39,29 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const letterCount = expr.length / 10;
+    let decodedString = '';
+    let letter = '';
+
+    for (let i = 0; i < letterCount; i++){
+      letter = expr.substring(0, 10);
+      let morseLetter = '';
+      if(letter === `**********`)
+        decodedString += ' ';
+      else{
+        for (let j = 0; letter !== 0 || j < 5; j++){
+          if (parseInt(letter) % 100 === 10)
+            morseLetter += `.`;
+          if (parseInt(letter) % 100 === 11)
+            morseLetter += `-`;
+            letter = parseInt(letter) / 100;
+        }
+        decodedString += MORSE_TABLE[morseLetter.split('').reverse().join('')];
+
+      }
+      expr = expr.substring(10);
+    }
+    return decodedString;
 }
 
 module.exports = {
